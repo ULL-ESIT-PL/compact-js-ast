4b37fa84ce45a014d154e83860e2ed852b385f29
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2023-03-06 20:10:23 +0000

    commander

--- cast.cjs
+++ cast.cjs
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-// outputs a compact string representation of the AST. Example:
+ // outputs a compact string representation of the AST. Example:
 // ✗ node cast3.cjs 'while (x == 1) {}' 0
 // [
 //   'Program',
@@ -15,29 +15,63 @@
 //   ]
 // ]
 const util = require("util");
+const fs = require("fs");
 const espree = require("espree");
 const astTypes = require("ast-types");
 const NodePath = astTypes.NodePath;
 
-const code =process.argv[2] || "a = 1; b = 2;";
-const ast = espree.parse(code, { ecmaVersion: espree.latestEcmaVersion });
+let {
+  program
+} = require("commander");
+const {
+  version
+} = require("./package.json");
 
-let past = new NodePath(ast);
-astTypes.visit(past, {
-  visitNode: function(path) {
-    path.cast = [ path.node.type ];
-    if (path.node.operator) {
-      path.cast.push(path.node.operator);
+program
+  .version(version)
+  .argument("[filename]", 'file with the original code')
+  .option("-o, --output <filename>", "file name of the JS input program")
+  .option("-p, --program <JSprogram>", "JS input program")
+  .action((filename, options) => {
+    if (options.program) main(options.program, options);
+    else if (filename) {
+      let code = fs.readFileSync(filename);
+      main(code, options);
     }
-    this.traverse(path);
-    if (path.node.name) {
-      path.cast.push(path.node.name);
-    } else if (path.node.value) {
-      path.cast.push(path.node.value);
+    else program.help()
+  });
+
+program.parse(process.argv);
+
+function main(code, options) {
+  //console.log("options=",options)
+  const ast = espree.parse(code, {
+    ecmaVersion: espree.latestEcmaVersion
+  });
+
+  let past = new NodePath(ast);
+  astTypes.visit(past, {
+    visitNode: function (path) {
+      path.cast = [path.node.type];
+      if (path.node.operator) {
+        path.cast.push(path.node.operator);
+      }
+      this.traverse(path);
+      if (path.node.name) {
+        path.cast.push(path.node.name);
+      } else if (path.node.value) {
+        path.cast.push(path.node.value);
+      }
+      path.parent?.cast.push(path.cast);
     }
-    path.parent?.cast.push(path.cast);
+  });
+
+  let stringast = util.inspect(past.cast, { depth: null });
+  if (options?.output) {
+    fs.writeFileSync(options.output, stringast);
+  } else {
+    console.log(stringast)
   }
-});
+}
 
-//console.log(JSON.stringify(past.cast, null, space));
-console.log(util.inspect(past.cast, { depth: null }))
+main()
